f1846bbca64ba3a1de035a1c50eba1d5eacab24d
Viktor Shabatin <shabatin.viktor@gmail.com>
2019-01-21 02:31:52 +0200

    initial commit

--- homeJS/5.js
+++
@@ -1,7 +0,0 @@
-var name = "Viktor";
-function myName(name) {
-	alert(name);
-}
-myName(); //Вернет undefined, так как значение аргумента не задано
-myName("Viktor"); // Вернет Viktor, так мы передали значение аргумента в функцию
-myName(name); // вернет Viktor, так как мы передали аргументу переменную со значением Viktor
